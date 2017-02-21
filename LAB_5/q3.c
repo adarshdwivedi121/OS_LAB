fad13ca03afbdf359fe8f05c9566257b6c236d31
adarsh <adarshdwivedi121@gmail.com>
2017-02-22 00:14:33 +0530

    Lab 5 codes added

--- LAB_5/q3.c
+++ LAB_5/q3.c
@@ -4,33 +4,36 @@
 
 int main(int argc, char *argv[]){
 	int pid;
+	int i, j;
 	pid=fork();
-	
-	int num = argc-1, i, j;
 	if (!pid){
+		char list[argc-1][20];
+		for (i=1; i<argc; i++)
+			strcpy(list[i-1], argv[i]);
+
+		char str[20];
+		int num = argc-1;
 		for (i=0; i<num; i++)
-			for (j=0; j<num-1-i; j++)
-				if(strcmp(argv[j], argv[j+1]) > 0){
-					char *str;
-					strcpy(str, argv[j]);
-					strcpy(argv[j], argv[j+1]);
-					strcpy(argv[j+1], str);
+			for (j=i; j<num-1-i; j++){
+				if(strcmp(list[j], list[j+1]) > 0){
+					strcpy(str, list[j]);
+					strcpy(list[j], list[j+1]);
+					strcpy(list[j+1], str);
 				}
-		
-		printf("\nSorted String");
+			}
+			
+		printf("\n\nSorted String");
 		for (i=0; i<num; i++)
-			printf("\n%s", argv[i]);
-		printf("\n");
+			printf("\n%s", list[i]);
 	}
-	
-	{
-    	wait();
-    	int i;
-	printf("\nGiven String");
-	for (i=0; i<num; i++)
-        	printf("\n%s", argv[i]);
-	printf("\n");
+
+	else{
+		wait(); 
+    	printf("\n\nGiven String");
+		for (i=1; i<argc; i++)
+	    	printf("\n%s", argv[i]);
     }
-               
+
+	printf("\n");
     return 0;
 }
