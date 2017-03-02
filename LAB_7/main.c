3bc16c3d15ca9181fe69023ab4f45ddc8b3e4e02
Adarsh <adarshdwivedi121@gmail.com>
2017-03-02 11:28:39 +0530

    LAB 6 codes added

--- LAB_7/main.c
+++ LAB_7/main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdlib.h>
+
 #define BUFFER_SIZE 10
 
 sem_t mayProduce, mayConsume;
-pthread_t prod[5], cons[5];
+pthread_t prod[BUFFER_SIZE], cons[BUFFER_SIZE];
 pthread_mutex_t mutex;
 int np, nc;
 int counter;
-int BUFFER[BUFFER_SIZE];
+int buffer[BUFFER_SIZE];
 
 void initialize();
 void *producer();
 void *consumer();
 
 int main() {
-    printf("\nEnter No. of Producers");
+    initialize();
+    printf("\nEnter No. of Producers : ");
     scanf("%d", &np);
-    printf("\nEnter No. of Consumers");
+    printf("\nEnter No. of Consumers : ");
     scanf("%d", &nc);
+
     int i;
     for(i=0; i<np; i++)
         pthread_create(&prod[i], NULL, producer, NULL);
@@ -28,8 +32,30 @@ int main() {
         pthread_join(&prod[i], NULL);
     for(i=0; i<nc; i++)
         pthread_join(&cons[i], NULL);
+
+    return 0;
 }
 
 void initialize(){
-    pthread_mutex_init(&mutex, )
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&mayProduce, 0, 0);
+    sem_init(&mayConsume, 0, BUFFER_SIZE);
+    counter = 0;
+}
+
+void *producer(){
+    sem_wait(&mayConsume);
+    pthread_mutex_lock(&mutex);
+    buffer[counter++] = counter;
+    pthread_mutex_unlock(&mutex);
+    sem_post(&mayProduce);
+}
+
+void *consumer(){
+    if(counter==0)  exit(0);
+    sem_wait(&mayProduce);
+    pthread_mutex_lock(&mutex);
+    printf("\nConsumer has Value : %d", buffer[--counter]);
+    pthread_mutex_unlock(&mutex);
+    sem_post(&mayConsume);
 }
